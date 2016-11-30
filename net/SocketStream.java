474bb9c4ac43583bfb85e6a48331d9aa9134f6a1
centixkadon <centixkadon@126.com>
2016-12-01 02:46:33 +0800

    Create class SocketStream to read & write Socket.

---
+++ net/SocketStream.java
@@ -0,0 +1,37 @@
+
+package net;
+
+import java.io.*;
+import java.net.Socket;
+
+public class SocketStream implements AutoCloseable {
+	private final PrintStream printer;
+	private final BufferedReader reader;
+	
+	public SocketStream(Socket socket) throws IOException {
+		printer = new PrintStream(socket.getOutputStream());
+		reader = new BufferedReader(
+			new InputStreamReader(socket.getInputStream()));
+	}
+	
+	public String readLine() {
+		try {
+			return reader.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void print(String str) {
+		printer.print(str);
+	}
+	
+	public void println(String str) {
+		printer.println(str);
+	}
+	
+	@Override
+	public void close() throws Exception {}
+}
