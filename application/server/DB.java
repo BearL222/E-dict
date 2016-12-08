4d9b488c1922ef613de7cb58b7c7f996353a98be
BearL222 <429274165@qq.com>
2016-12-08 16:20:09 +0800

    database
    database done, including user signin, signout, signup and number of zan.

--- application/server/DB.java
+++ application/server/DB.java
@@ -18,7 +18,7 @@ class DB {
 		Statement statement = connection.createStatement();
 
 		ResultSet resultSet = statement.executeQuery("SELECT * FROM ZAN WHERE WORD = \'" + word + "\'");
-		// 是否已经存有该单词
+		// 此时未存有该单词
 		if (!resultSet.next()) {
 			statement.executeUpdate("INSERT INTO ZAN(WORD,ZAN0,ZAN1,ZAN2)VALUES(\'" + word + "\',0,0,0)");
 		}
@@ -27,7 +27,7 @@ class DB {
 
 	}
 
-	// 获得该单词赞数
+	// 返回该单词赞数
 	public int getZan(int dict, String word) throws SQLException {
 		int result;
 
@@ -43,6 +43,63 @@ class DB {
 			result = Integer.valueOf(resultSet.getString(1));
 		}
 
+		connection.close();
+		return result;
+	}
+
+	// 用户登陆（返回：0：成功登陆；1：密码错误；2：用户不存在）
+	public int signinUser(String name, String passcode) throws SQLException {
+		int result = 0;
+		Connection connection = DriverManager.getConnection(url, user, password);
+		Statement statement = connection.createStatement();
+
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM USER_INFO WHERE UNAME = \'" + name + "\'");
+
+		if (resultSet.next()) {
+			if (resultSet.getString(2).replace(" ", "").equals(passcode)) {
+				// 登陆成功
+				result = 0;
+				statement.executeUpdate("UPDATE USER_INFO SET ONLINE = " + 1 + " WHERE UNAME = \'" + name + "\'");
+			} else {
+				// 密码错误
+				result = 1;
+			}
+
+		} else {
+			// 用户不存在
+			result = 2;
+		}
+		connection.close();
+		return result;
+	}
+
+	//用户下线
+	public int signoutUser(String name) throws SQLException {
+		int result = 0;
+		Connection connection = DriverManager.getConnection(url, user, password);
+		Statement statement = connection.createStatement();
+		statement.executeUpdate("UPDATE USER_INFO SET ONLINE = 0 WHERE UNAME = \'" + name + "\'");
+
+		connection.close();
+		return result;
+	}
+	
+	// 用户注册（返回：0：注册成功；1：用户名已被占用）
+	public int signupUser(String name, String passcode) throws SQLException {
+		int result = 0;
+		Connection connection = DriverManager.getConnection(url, user, password);
+		Statement statement = connection.createStatement();
+
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM USER_INFO WHERE UNAME = \'" + name + "\'");
+
+		if (resultSet.next()) {
+			// 该用户名已存在
+			result = 1;
+		} else {
+			statement.executeUpdate(
+					"INSERT INTO USER_INFO(UNAME,UCODE,ONLINE)VALUES(\'" + name + "\'," + passcode + ",0)");
+			result = 0;
+		}
 		connection.close();
 		return result;
 	}
