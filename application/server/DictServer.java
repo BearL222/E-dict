9f14d3e0e429998c180c5a5c16f4a0563149c3bd
centixkadon <centixkadon@126.com>
2016-12-19 21:49:30 +0800

    Fix bugs

--- application/server/DictServer.java
+++ application/server/DictServer.java
@@ -10,8 +10,13 @@ public class DictServer {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		try (ServerSocket server = new ServerSocket(Connect.SERVER_PORT)) {
-			while (true) /*for (int i = 0; i < 10; ++i)*/ {
-				new DictRespond(server.accept(), null/*new DB()*/).run();
+			while (true) { /* for (int i = 0; i < 10; ++i) */
+				// new DictRespond(server.accept(), null).run();
+				try {
+					new DictRespond(server.accept(), new DB()).run();
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
